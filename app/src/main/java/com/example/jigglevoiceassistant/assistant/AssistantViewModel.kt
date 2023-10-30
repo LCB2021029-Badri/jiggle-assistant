58d1272342d51d26347a87a1ed88ac8380bea995
badriakkala <lcb2021029@iiitl.ac.in>
2023-10-30 12:26:40 +0530

    performing the functions of DAO in viewmodel

---
+++ app/src/main/java/com/example/jigglevoiceassistant/assistant/AssistantViewModel.kt
@@ -0,0 +1,84 @@
+package com.example.jigglevoiceassistant.assistant
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.jigglevoiceassistant.data.Assistant
+import com.example.jigglevoiceassistant.data.AssistantDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class AssistantViewModel (val database: AssistantDao, application: Application) : AndroidViewModel(application) {
+
+    private var viewModelJob = Job()
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private var currentMessage = MutableLiveData<Assistant?>()
+
+    val messages = database.getAllMessages()
+
+    init{
+        initializeCurrentMessage()
+    }
+
+    private fun initializeCurrentMessage() {
+        uiScope.launch {
+            currentMessage.value = getCurrentMessageFromDatabase()
+        }
+    }
+
+    private suspend fun getCurrentMessageFromDatabase(): Assistant? {
+        return withContext(Dispatchers.IO){
+            var message = database.getCurrentMessage()
+            if(message?.assistant_message == "DEFAULT_MESSAGE" || message?.human_message == "DEFAULT_MESSAGE" ) {
+                message = null
+            }
+            message
+        }
+    }
+
+    fun sendMessageToDatabase(assistantMessage: String, humanMessage: String){
+        uiScope.launch {
+            val newAssistant = Assistant()
+            newAssistant.assistant_message = assistantMessage
+            newAssistant.human_message = humanMessage
+            insert(newAssistant)
+            currentMessage.value = getCurrentMessageFromDatabase()
+        }
+    }
+
+    private suspend fun insert(message: Assistant){
+        withContext(Dispatchers.IO){
+            database.insert(message)
+        }
+    }
+
+    private suspend fun update(message: Assistant) {
+        withContext(Dispatchers.IO) {
+            database.update(message)
+        }
+    }
+
+    fun onClear() {
+        uiScope.launch {
+            clear()
+            currentMessage.value = null
+        }
+    }
+
+    private suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+
+
+}
