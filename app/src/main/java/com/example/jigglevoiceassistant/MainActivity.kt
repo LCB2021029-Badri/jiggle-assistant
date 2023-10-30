1473b0f1e514f03554d189cd7dd37d3fbd86f034
badriakkala <lcb2021029@iiitl.ac.in>
2023-10-30 11:17:01 +0530

    manifest permisions and setup

--- app/src/main/java/com/example/jigglevoiceassistant/MainActivity.kt
+++ app/src/main/java/com/example/jigglevoiceassistant/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.jigglevoiceassistant
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.jigglevoiceassistant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val RECORD_AUDIO_REQUEST_CODE = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn1.setOnClickListener {
+            checkPermission()
+        }
+
     }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            // Request permission if not granted
+            requestAudioPermission()
+        } else {
+            startAssistantActivity()
+        }
+    }
+
+    private fun requestAudioPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                RECORD_AUDIO_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun startAssistantActivity() {
+        startActivity(Intent(this, AssistantActivity::class.java))
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == RECORD_AUDIO_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            startAssistantActivity()
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/jigglevoiceassistant/assistant/AssistantActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jigglevoiceassistant.assistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jigglevoiceassistant.R
+
+class AssistantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_assistant)
+    }
+}
